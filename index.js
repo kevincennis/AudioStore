2f02c9c5ecb2f63224d47ee78b0dcf07be27e4cb
Kevin Ennis <kennis84@gmail.com>
2017-03-05 12:18:36 -0500

    Add logs to UI

--- index.js
+++ index.js
@@ -3,6 +3,16 @@ const el       = document.querySelector('.player');
 const ac       = new AudioContext();
 const store    = new AudioStore( ac );
 const streamer = new Streamer( url, store );
+const logs     = document.querySelector('.logs');
+const info     = console.info;
+
+console.info = str => {
+  let txt = logs.textContent;
+  txt += `${ str }\n`;
+  logs.textContent = txt;
+  logs.scrollTop = Math.pow( 2, 53 ) - 1;
+  info.call( console, str );
+};
 
 store.init()
 .then( () => streamer.load() )
